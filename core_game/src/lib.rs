e8e2562611c7d71ffdbf1db44856554bd4d4e967
Rajat Rajput <rajputrajat@gmail.com>
2021-07-25 17:02:14 +0530

    moved the req into readme.md file from lib module

--- core_game/src/lib.rs
+++ core_game/src/lib.rs
@@ -1,12 +1,4 @@
 //! # Core game logic goes in here
-//! Let's list down required functionalities first
-//! * 2 - 4 players can play
-//! * server can join in play
-//! * dice has 6 faces
-//! * play starts with 1 or 6
-//! * if a player's piece overlaps other player's piece.
-//! * * overlapped one has to restart
-//! * * the one overlapped gets one more chance to roll the dice
 //!
 #![deny(unsafe_code)]
 #![deny(missing_docs)]
