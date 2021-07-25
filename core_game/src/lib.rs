be40ec036d44186bd52e235e89a418e21ab0ed49
Rajat Rajput <rajputrajat@gmail.com>
2021-07-25 16:53:12 +0530

    add core_game crate

--- core_game/src/lib.rs
+++ core_game/src/lib.rs
@@ -1,7 +1,9 @@
+//! Core game logic goes in here
+#![deny(unsafe_code)]
+#![deny(missing_docs)]
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+    fn it_works() {}
 }
