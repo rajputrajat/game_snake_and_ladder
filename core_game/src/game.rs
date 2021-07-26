6dd7620cd462d3eb56b2eed918cdbf141d8c074f
Rajat Rajput <rajputrajat@gmail.com>
2021-07-26 12:57:32 +0530

    wip design added

--- core_game/src/game.rs
+++ core_game/src/game.rs
@@ -1,2 +1,8 @@
 /// Game
-pub(crate) struct Game {}
+use crate::board::{Board, Entity};
+use crate::player::{Player, PlayerState};
+
+pub struct Game {
+    board: Board,
+    players: Vec<Player>,
+}
