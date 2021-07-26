642bc3340a539031bb3a37d2e0729980696979f1
Rajat Rajput <rajputrajat@gmail.com>
2021-07-26 19:43:50 +0530

    wip. adding lib functions

--- core_game/src/lib.rs
+++ core_game/src/lib.rs
@@ -4,12 +4,31 @@
 #![warn(missing_docs)]
 
 mod abilities;
-pub mod board;
+mod board;
 mod cell;
 mod entity;
 mod game;
 mod misc;
 mod player;
+
+pub use crate::player::{Player, PlayerAction, PlayerId};
+use anyhow::Result;
+
+pub struct GameCore {
+    pub players: Vec<PlayerId>,
+}
+
+impl GameCore {
+    pub fn update() {}
+
+    pub fn add_player(player: Player) -> PlayerId {}
+
+    pub fn remove_player(player_id: PlayerId) -> Result<()> {}
+
+    pub fn create_board(side_length: SideLength) -> Result<()> {}
+
+    pub fn action(player_id: PlayerId, player_action: PlayerAction) {}
+}
 
 #[cfg(test)]
 mod tests {
--- core_game/src/player.rs
+++ core_game/src/player.rs
@@ -1,5 +1,6 @@
 /// Player
 use crate::abilities::Ability;
+use crate::entity::Movement;
 
 pub(crate) struct PlayerId(u8);
 
@@ -10,8 +11,16 @@ pub(crate) struct Player {
     abilities: Vec<Ability>,
 }
 
+pub(crate) struct SuperDice(pub(crate) u8);
+
 pub(crate) enum PlayerState {
     Idle,
     InPlay,
     Won,
+}
+
+pub(crate) enum PlayerAction {
+    RollDice,
+    RollSuperDice(SuperDice),
+    MakeCustomEntity(Movement),
 }
