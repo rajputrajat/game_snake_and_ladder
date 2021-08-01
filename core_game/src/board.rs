76c25493ea8a8231001dfcd4b0d2c2c610aefa1c
Rajat Rajput <rajputrajat@gmail.com>
2021-08-01 09:06:45 +0530

    built with warinings. remove used ability

--- core_game/src/board.rs
+++ core_game/src/board.rs
@@ -80,11 +80,22 @@ impl Board {
                     &PlayerAction::RollDice(dice) => {
                         let outcome = self.roll_dice(&dice);
                     }
-                    &PlayerAction::UseAbility(ability) => match &ability {
-                        &Ability::SuperDice(dice) => self.get_player_mut(player_id)?.
-                    },
+                    &PlayerAction::UseAbility(ability) => {
+                        match &ability {
+                            &Ability::SuperDice(dice) => {}
+                            &Ability::CustomSnLdMaker(Some(movement)) => {}
+                            _ => {}
+                        }
+                        self.get_player_mut(player_id)?.remove_ability(ability)?;
+                    }
                 },
-                &StateMachine::Move => {}
+                &StateMachine::Move => {
+                    break;
+                }
+                &StateMachine::Init => {}
+                &StateMachine::CheckEntity => {}
+                &StateMachine::CheckOverlap => {}
+                &StateMachine::Win => {}
             }
         }
         Ok(())
